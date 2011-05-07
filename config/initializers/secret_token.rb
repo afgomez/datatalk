19056afcff90c11b73fc99ae11ebba475563d265
Alejandro Fernández <alejandro@simplelogica.net>
2011-05-07 12:29:53 +0200

    Rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datatalk::Application.config.secret_token = '011f9b758455215dcd187505850d162919bcb2e7ee4854fe82d5e98c79f283304baa6f5770c7496241fb30c2aec3a1aa9f07ef95ddde00823b8aa712b11b37be'
