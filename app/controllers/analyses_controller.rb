2f804f5af03a4095a2134373bed4aafbd243e5ed
Rodrigo Garcia Suarez <rodrigo@simplelogica.net>
2011-05-08 21:18:30 +0200

    Added Analysis model edition form.

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
 class AnalysesController < ApplicationController
-  
-  before_filter :logged_in?,  :only => [:new, :create]
+
+  before_filter :authenticate_user!, :except => [:index, :show]
+  before_filter :validate_owner, :only => [:edit, :update]
 
   def index
     @analyses = Analysis.page(params[:page]).per(10)
   end
-    
+
   def show
     @analysis = Analysis.find(params[:id])
   end
@@ -14,7 +16,7 @@ class AnalysesController < ApplicationController
     @analysis = Analysis.new
     @analysis.parent = Analysis.find(params[:reply]) unless params[:reply].nil?
   end
-  
+
   def create
     @analysis = Analysis.new params[:analysis]
     @analysis.user_id = current_user.id
@@ -27,8 +29,27 @@ class AnalysesController < ApplicationController
       render :action => :new
     end
   end
-  
+
+  def edit
+    @analysis = Analysis.find(params[:id])
+  end
+
+  def update
+    @analysis = Analysis.find(params[:id])
+    @analysis.update_attributes(params[:analysis])
+    if @analysis.valid?
+      flash[:notice] = "El análisis se ha actualizado."
+      render :show
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def validate_owner
+    render :text => '¿Intentando editar el análisis de otro usuario? Tsk, tsk, tsk...' unless Analysis.find(params[:id]).user.id == current_user.id
+  end
 
   def logged_in?
     if current_user.nil?
