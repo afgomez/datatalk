316a812306f0c1f8c1bf5994ddc4255f02d9d526
David J. Brenes <david.brenes@simplelogica.net>
2011-05-08 14:30:28 +0200

    Created visualizations' admin with the refinery generator and left with the minimum amount of code

--- config/routes.rb
+++ config/routes.rb
@@ -43,12 +43,13 @@ Datatalk::Application.routes.draw do
   #   end
 
   # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
+  scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
+    resources :visualizations, :except => :show do
+      collection do
+        post :update_positions
+      end
+    end
+  end
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => "home#index"
---
+++ vendor/engines/visualizations/refinerycms-visualizations.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |s|
+  s.platform          = Gem::Platform::RUBY
+  s.name              = 'refinerycms-visualizations'
+  s.version           = '1.0'
+  s.description       = 'Ruby on Rails Visualizations engine for Refinery CMS'
+  s.date              = '2011-05-08'
+  s.summary           = 'Visualizations engine for Refinery CMS'
+  s.require_paths     = %w(lib)
+  s.files             = Dir['lib/**/*', 'config/**/*', 'app/**/*']
+end
