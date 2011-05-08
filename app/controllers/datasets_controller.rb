43a6859cc79f2a6a75e43db52258ed34af1347b4
David J. Brenes <david.brenes@simplelogica.net>
2011-05-08 22:29:09 +0200

    Including flash messages to the forms

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -19,10 +19,20 @@ class DatasetsController < ApplicationController
   def create
     @dataset = Dataset.create(params[:dataset].merge(:user => current_user))
     if @dataset.valid?
+      flash[:notice] = "Has creado un nuevo conjunto de datos. ¡Gracias!"
       render :show
     else
+      flash[:alert] = "Ha habido algún problema con la creación del fichero de datos."
       render :new
     end
   end
 
+  private 
+
+  def logged_in?
+    if current_user.nil?
+      flash[:alert] = "No puedes subir ficheros de datos si no estás registrado"
+      redirect_to new_user_session_path      
+    end
+  end
 end
